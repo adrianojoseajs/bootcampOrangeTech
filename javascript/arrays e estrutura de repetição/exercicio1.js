14130859fd23d7cabac3e27d20bf9d9db6e63b41
Adriano José da Silva <adriano.silva@modalgr.com.br>
2022-12-08 16:20:19 -0300

    exercicio 1 array e estrutura de repetição

---
+++ javascript/arrays e estrutura de repetição/exercicio1.js
@@ -0,0 +1,8 @@
+/* 1- Crie um programa que dado um número imprime sua tabuada */
+
+const numero = 2;
+let tabuada;
+for (let index = 1; index <= 10; index++) {
+    console.log (numero * index);
+
+}
